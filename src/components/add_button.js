03e767a3cbe1783b686fbfefef244ed45a94e43d
Sam <samwentupp@outlook.com>
2023-10-21 14:10:24 -0700

    app.js changes

--- src/components/add_button.js
+++ src/components/add_button.js
@@ -1,7 +1,13 @@
-import { useState } from 'react'
-import {IoAddCircleOutline} from 'react-icons/io5'
+import { useState, useContext } from 'react'
+import { IoAddCircleOutline } from 'react-icons/io5'
 
 export default function AddButton(){
+    const [formOpen, setFormOpen] = useState(false); 
+
+    function onClick(){
+        
+    }
+
     return (
         <div className='flex gap-2 items-center w-16 p-1 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-400 transition-all'>
             <span>Add</span>
