d283d2e92225849c0abad25ac281d831fa211642
Sam <samwentupp@outlook.com>
2023-10-21 11:19:55 -0700

    init code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import AddButton from "./components/add_button";
+import Board from "./components/board";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AddButton></AddButton>
+      <Board></Board>
+      <Navbar></Navbar>
+    </>
   );
 }
 
---
+++ src/components/add_button.js
@@ -0,0 +1,5 @@
+export default function AddButton(){
+    return (
+        <>Hello Button</>
+    )
+}
