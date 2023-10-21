c48a7af70882ddc9f2a5df7e69cffaec4d5d8b55
Roy Shi <royshi@d142-058-087-019.wireless.sfu.ca>
2023-10-21 15:01:07 -0700

    navbar + board

--- src/components/board.js
+++ src/components/board.js
@@ -1,5 +1,26 @@
 export default function Board(){
+
+    const styles = {
+        column: {
+            float: "left",
+            width: "33.3%"
+        }
+    }
     return (
-        <>Helo board</>
+        <>
+            <main className='flex justify-center w-4/5 ml-72'>
+                <section style={styles.column}>
+                    Went Well
+                </section>
+                <section style={styles.column}>
+                    To Improve
+                </section>
+                <section style={styles.column}>
+                    Action Items
+                </section>
+            </main>
+            <br></br><br></br>
+            Helo board
+        </>
     )
 }
