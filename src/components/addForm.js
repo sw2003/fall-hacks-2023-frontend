7797dd5ae0e514ed0e2bb7c70d8a4ad1fa958dbd
Sam <samwentupp@outlook.com>
2023-10-21 20:37:54 -0700

    .

--- src/components/addForm.js
+++ src/components/addForm.js
@@ -73,7 +73,7 @@ export default function AddForm() {
                         <form method="post" onSubmit={handleSubmit}>
 
                             <div className="relative w-full flex justify-center my-2 opacity-1">
-                                <div onClick={onClick} className={`flex items-center gap-3 ${insightStatus === '' && 'bg-blue-500'} ${insightStatus === 'Went Well' && 'bg-teal-500'} ${insightStatus === 'To Improve' && 'bg-rose-400'} ${insightStatus === 'Action Items' && 'bg-violet-600'} p-2 rounded-lg cursor-pointer my-2 text-white`}>
+                                <div onClick={onClick} className={`flex items-center gap-3 ${insightStatus === '' && 'bg-blue-500'} ${insightStatus === 'Went Well' && 'bg-green-400'} ${insightStatus === 'To Improve' && 'bg-yellow-500'} ${insightStatus === 'Action Items' && 'bg-rose-500'} p-2 rounded-lg cursor-pointer my-2 text-white`}>
                                     
                                     {
                                         insightStatus === '' && "Select Insight Status"
@@ -88,21 +88,20 @@ export default function AddForm() {
                             {
                                 showInsightOptions && 
                                 <div className="w-2/5 mx-auto flex flex-col gap-1">
-                                    <div className="p-1 bg-teal-500 text-white rounded-md cursor-pointer hover:pl-6 transition-all" onClick={clickStatus}>Went Well</div>
-                                    <div className="p-1 bg-rose-400 text-white rounded-md cursor-pointer hover:pl-6 transition-all" onClick={clickStatus}>To Improve</div>
-                                    <div className="p-1 bg-violet-600 text-white rounded-md cursor-pointer hover:pl-6 transition-all" onClick={clickStatus}>Action Items</div>
+                                    <div className="p-1 bg-green-400 text-white rounded-md cursor-pointer hover:pl-6 transition-all" onClick={clickStatus}>Went Well</div>
+                                    <div className="p-1 bg-yellow-500 text-white rounded-md cursor-pointer hover:pl-6 transition-all" onClick={clickStatus}>To Improve</div>
+                                    <div className="p-1 bg-rose-500 text-white rounded-md cursor-pointer hover:pl-6 transition-all" onClick={clickStatus}>Action Items</div>
                                 </div>
                             }
 
+                            <div className="block text-center"> Enter content: </div>
                             <div className="w-full flex justify-center">
-                                <label> Enter content: </label>
                                 <textarea name="contentInput" type="text" className="resize-none w-3/4 h-28 my-2 mx-auto border shadow-lg border-slate-200" value={content} onChange={textBoxChange} ></textarea>
                                 {/* <input type="text" className="ml-2 border w-3/4" value={content} onChange={textBoxChange}/> */}
-                                
                             </div>
 
                             <div className="flex justify-center">
-                                <button className="border text-white bg-blue-500 rounded hover:bg-blue-400 cursor-pointer flex gap-2 transition-all w-16 p-1" type="submit"> 
+                                <button className="border text-white bg-green-600 rounded hover:bg-green-500 cursor-pointer flex gap-2 transition-all w-16 p-1" type="submit"> 
                                     Submit 
                                     <span><BiSubdirectoryLeft></BiSubdirectoryLeft></span>
                                 </button>
