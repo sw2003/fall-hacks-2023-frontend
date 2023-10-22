3d29b70ae0390372321e5c6a768c666394b8005b
Roy <pipiroy03@gmail.com>
2023-10-21 18:24:17 -0700

    proxy fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { createContext, useState } from 'react' 
 
+import './App.css';
+
 import AddButton from "./components/add_button";
 import Board from "./components/board";
 import Navbar from "./components/navbar";
 import AddForm from './components/addForm';
 
 export const boardContext = createContext(null);
-
-import './App.css';
 
 
 function App() {
